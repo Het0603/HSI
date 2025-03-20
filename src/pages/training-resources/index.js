906caefb19ebf70114ce574e0288ca0fe976eee8
nikunjj1175 <nikunjkorat117@gmail.com>
2025-03-20 15:52:29 +0530

    training resources route add

---
+++ src/pages/training-resources/index.js
@@ -0,0 +1,13 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import YouTubePlaylist from "@/components/YouTubePlaylist";
+
+export default function TrainingResources() {
+     return (
+          <div>
+               <Header />
+               <YouTubePlaylist />
+               <Footer />
+          </div>
+     )
+}
