4ec06a495db74eba1377b0b5b37ef13b3251b51e
nikunjj1175 <nikunjkorat117@gmail.com>
2025-04-01 16:56:02 +0530

    meta data set in website

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,10 +8,22 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Humane World Apps</title>
         <meta name="description" content="Empowering Animal Welfare Through Technology Driven Innovation" />
+        <meta name="keywords" content="Animal Welfare, Technology, Humane World, Innovation, Apps for Animals" />
+        <meta name="author" content="Humane World Apps" />
+        
         <meta property="og:title" content="Humane World Apps" />
         <meta property="og:description" content="Empowering Animal Welfare Through Technology Driven Innovation" />
-        <meta property="og:image" content="/blog1.jpg" />
+        <meta property="og:image" content="https://images.unsplash.com/photo-1560807707-8cc77767d783" />
         <meta property="og:image:alt" content="Humane World Apps Logo or Image" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.yourwebsite.com" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Humane World Apps" />
+        <meta name="twitter:description" content="Empowering Animal Welfare Through Technology Driven Innovation" />
+        <meta name="twitter:image" content="https://images.unsplash.com/photo-1560807707-8cc77767d783" />
+        <meta name="twitter:image:alt" content="Humane World Apps Logo or Image" />
+        <meta name="twitter:site" content="@YourTwitterHandle" />
       </Head>
       <Component {...pageProps} />
       <ScrollToTopButton />
