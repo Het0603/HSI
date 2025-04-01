670e250b326ddb974bef130105c4a95f967a7a04
nikunjj1175 <nikunjkorat117@gmail.com>
2025-04-01 17:26:09 +0530

    meta data change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,18 +12,17 @@ function MyApp({ Component, pageProps }) {
         <meta name="author" content="Humane World Apps" />
         
         <meta property="og:title" content="Humane World Apps" />
-        <meta property="og:description" content="Empowering Animal Welfare Through Technology Driven Innovation" />
+        <meta property="og:description" content="Empowering Animal Welfare Through Technology Driven Innovation." />
         <meta property="og:image" content="https://images.unsplash.com/photo-1560807707-8cc77767d783" />
         <meta property="og:image:alt" content="Humane World Apps Logo or Image" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.yourwebsite.com" />
+        <meta property="og:url" content="https://human-animal-1.vercel.app/" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Humane World Apps" />
         <meta name="twitter:description" content="Empowering Animal Welfare Through Technology Driven Innovation" />
         <meta name="twitter:image" content="https://images.unsplash.com/photo-1560807707-8cc77767d783" />
         <meta name="twitter:image:alt" content="Humane World Apps Logo or Image" />
-        <meta name="twitter:site" content="@YourTwitterHandle" />
       </Head>
       <Component {...pageProps} />
       <ScrollToTopButton />
