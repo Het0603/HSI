e6845a0338b85cd21ce3f04adb985d8e7164a89a
nikunjj1175 <nikunjkorat117@gmail.com>
2025-04-13 11:01:28 +0530

    top button changes & header text changes

--- src/components/ScrollToTopButton.js
+++ src/components/ScrollToTopButton.js
@@ -29,7 +29,7 @@ const ScrollToTopButton = () => {
   return (
     <button
       onClick={scrollToTop}
-      className={`fixed flex justify-center items-center bottom-10 right-10 p-3 sm:p-4 md:p-5 w-12 sm:w-14 md:w-14 h-12 sm:h-14 md:h-14 bg-[#00352C] text-white rounded-full shadow-2xl hover:shadow-2xl hover:bg-[#005a3c] focus:outline-none transform transition-all duration-300 ${
+      className={`fixed flex justify-center z-10 border-2 border-white items-center bottom-10 right-10 p-3 sm:p-4 md:p-5 w-12 sm:w-14 md:w-14 h-12 sm:h-14 md:h-14 bg-[#00352C] text-white rounded-full shadow-2xl hover:shadow-2xl hover:bg-[#005a3c] focus:outline-none transform transition-all duration-300 ${
         isVisible ? 'opacity-100' : 'opacity-0'
       }`}
     >
