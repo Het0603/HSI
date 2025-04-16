89bfe33bf3166f65d416b1616e04780b33623ae2
nikunjj1175 <nikunjkorat117@gmail.com>
2025-04-16 15:24:06 +0530

    first time open popup desing change

--- src/components/FirstTimeModal.js
+++ src/components/FirstTimeModal.js
@@ -1,5 +1,7 @@
 'use client';
+import Image from 'next/image';
 import { useState, useEffect } from 'react';
+import hero1 from "../../public/hero4.jpg";
 
 export default function FirstTimeModal() {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,27 +16,32 @@ export default function FirstTimeModal() {
 
   return (
     isOpen && (
-      <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="fixed inset-0 flex items-center justify-center px-4 sm:px-12 md:px-16 lg:px-24 z-50">
         <div className="absolute inset-0 bg-black opacity-50"></div>
-        <div className="bg-white max-w-4xl w-full relative flex">
-          <div className="w-1/2 p-6 flex flex-col justify-center gap-10">
-            <h2 className="text-3xl text-center font-bold mb-2 text-[#293941]">End animal cruelty</h2>
-            <p className="text-gray-700 mb-4">
-              For every animal saved, countless others are still suffering. Your donation can create a future
-              where animals no longer have to suffer cruelty and abuse.
-            </p>
-            <button onClick={() => setIsOpen(false)} className="bg-red-600 text-white px-4 py-2 rounded-md font-semibold w-full cursor-pointer hover:bg-red-700 transition-colors duration-200">
-              Start Saving Lives
-            </button>
-          </div>
-          <div className="w-1/2 relative">
-            <img
-              src="https://images.unsplash.com/photo-1560807707-8cc77767d783"
-              alt="Rescued Dog"
-              className="w-full h-full object-cover"
+        <div className="max-w-4xl w-full relative">
+          <div className="relative w-full h-[450px] sm:h-[450px] md:h-[450px] lg:h-[550px] overflow-hidden">
+            <Image
+              src={hero1}
+              alt="Hero Image"
+              className="absolute inset-0 w-full h-full object-cover filter contrast-125 brightness-70"
             />
+
+            <div className="flex flex-col justify-center absolute inset-0 bg-opacity-50 px-6 sm:px-10 md:px-16 lg:px-24 text-white">
+              <p className="text-lg sm:text-xl md:text-2xl max-w-[38rem]">
+                <span className="text-2xl lg:text-4xl">End animal cruelty</span>
+                <br />
+                <br />
+                For every animal saved, countless others are still suffering. Your donation can create a future
+                where animals no longer have to suffer cruelty and abuse.
+              </p>
+              <button onClick={() => setIsOpen(false)} style={{ fontFamily: "Noto Sans" }}
+                className="w-full sm:w-[35%] mt-6 bg-red-600 px-5 sm:px-6 py-2 rounded-lg sm:rounded-xl font-medium text-white hover:bg-red-700 text-base sm:text-lg md:text-xl transition-colors duration-200"
+              >
+                Start Saving Lives
+              </button>
+            </div>
             <button
-              className="absolute top-2 right-2 text-white cursor-pointer hover:bg-gray-500 rounded-full px-1"
+              className="absolute top-2 right-2 text-white cursor-pointer text-2xl hover:text-gray-500 rounded-full px-1"
               onClick={() => setIsOpen(false)}
             >
               ✖
